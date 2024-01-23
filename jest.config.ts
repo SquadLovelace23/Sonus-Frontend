6158209a52a246552cd5df7af5f762bcdfa6d787
Antonyo Andrei <antonyoandrei7@gmail.com>
2024-01-23 12:52:42 +0100

    jest config update

--- jest.config.ts
+++ jest.config.ts
@@ -7,24 +7,24 @@ import type { Config } from 'jest';
 
 const config: Config = {
   // Indicates whether the coverage information should be collected while executing the test
-  collectCoverage: true,
+  // collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  collectCoverageFrom: [
-    'src/**/*.{js,jsx,ts,tsx}',
-    '!src/**/*.d.ts',
-    '!src/index.tsx',
-    '!src/serviceWorker.ts',
-  ],
+  // collectCoverageFrom: [
+  //   'src/**/*.{js,jsx,ts,tsx}',
+  //   '!src/**/*.d.ts',
+  //   '!src/index.tsx',
+  //   '!src/serviceWorker.ts',
+  // ],
 
   // The directory where Jest should output its coverage files
-  coverageDirectory: 'coverage',
+  // coverageDirectory: 'coverage',
 
   // Indicates which provider should be used to instrument code for coverage
-  coverageProvider: 'v8',
+  // coverageProvider: 'v8',
 
   // A list of reporter names that Jest uses when writing coverage reports
-  coverageReporters: ['json', 'lcov', 'text', 'clover'],
+  // coverageReporters: ['json', 'lcov', 'text', 'clover'],
 
   // The test environment that will be used for testing React components
   testEnvironment: 'jsdom',
@@ -37,18 +37,18 @@ const config: Config = {
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   moduleNameMapper: {
-    '\\.(css|less|scss|sass)$': "<rootDir>/__mocks__/styleMock.ts",
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.ts',
+    '\\.(css|less|scss|sass)$': "<rootDir>/src/__mocks__/styleMock.ts",
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/src/__mocks__/fileMock.ts',
   },
 
   // An array of regexp pattern strings, matched against all source file paths, matched files will skip transformation
   transformIgnorePatterns: ['<rootDir>/node_modules/'],
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
-  unmockedModulePathPatterns: ['<rootDir>/node_modules/react/', '<rootDir>/node_modules/enzyme/'],
+  // unmockedModulePathPatterns: ['<rootDir>/node_modules/react/', '<rootDir>/node_modules/enzyme/'],
 
   // Setup Enzyme for React component testing
-  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+  // setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
 
   // Transform files with ts-jest
   transform: {
---
+++ src/tests/Player.test.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { render, fireEvent, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Player from "../components/Player/Player";
+
+jest.mock("../context/PlayerContext", () => ({
+  usePlayer: () => ({
+    currentSong: {
+      url: "mocked-audio-url",
+    },
+    setSong: jest.fn(),
+    isShuffled: false,
+  }),
+}));
+
+describe('Player component', () => {
+    test('renders play, pause, forward and previous buttons', () => {
+        render(<Player />);
+
+        screen.debug()
+    })
+})
