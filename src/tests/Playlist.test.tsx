75c8b159abac70bccd25b75a37b60604c9ec1cf9
Antonyo Andrei <antonyoandrei7@gmail.com>
2024-01-25 10:13:08 +0100

    playlist test

--- src/tests/Playlist.test.tsx
+++ src/tests/Playlist.test.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
-import { render, fireEvent, waitFor, act, screen } from '@testing-library/react';
+import { render, waitFor, act, screen } from '@testing-library/react';
 import { uploadRequest } from '../services/upload.service';
 import { createPlaylistByUserId } from '../services/playlist.service';
 import PlaylistModal from '../components/PlaylistModal/PlaylistModal';
 
 jest.mock("../services/upload.service", () => ({
-    uploadRequest: jest.fn(),
-  }));
+  uploadRequest: jest.fn(),
+}));
 
 jest.mock("../services/playlist.service", () => ({
-    createPlaylistByUserId: jest.fn(),
-    addSongToPlaylist: jest.fn(),
-    deleteSongFromPlaylist: jest.fn(),
-  }));
+  createPlaylistByUserId: jest.fn(),
+  addSongToPlaylist: jest.fn(),
+  deleteSongFromPlaylist: jest.fn(),
+}));
 
 describe('PlaylistModal', () => {
   test('renders without crashing', async () => {
+    (uploadRequest as jest.Mock).mockResolvedValue('mockImageUrl');
 
-      (uploadRequest as jest.Mock).mockResolvedValue('mockImageUrl');
-
-      (createPlaylistByUserId as jest.Mock).mockResolvedValue({
+    (createPlaylistByUserId as jest.Mock).mockResolvedValue({
       id: '1',
       name: 'Test Playlist',
       cover: 'mockImageUrl',
@@ -35,19 +34,13 @@ describe('PlaylistModal', () => {
     const updatePlaylists = jest.fn();
 
     await act(async () => {
-    const { getByPlaceholderText, getByRole } = 
-        render(
-          <PlaylistModal updatePlaylists={updatePlaylists} />
-        
-      );
-
-      fireEvent.click(getByRole('button', { name: /create/i }));
-      fireEvent.change(getByPlaceholderText(/Name.../i), { target: { value: 'Test Playlist' } });
-      fireEvent.click(getByRole('button', { name: /create/i }));
-
-      await waitFor(() => expect(updatePlaylists).toHaveBeenCalled());      
+      render(<PlaylistModal updatePlaylists={updatePlaylists} />);
     });
-  });
-  screen.debug();
 
+    await waitFor(() => {
+      expect(updatePlaylists).toHaveBeenCalled();
+    });
+
+    screen.debug();
+  });
 });
